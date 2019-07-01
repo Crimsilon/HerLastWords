fed3dc0975c455a949c8b2971a2222fee194c373
rikscamp <48299053+rikscamp@users.noreply.github.com>
2019-07-01 15:14:42 -0400

    beginning of stats
    implemented basic stats such as health, armour and damage.

---
+++ Assets/Scripts/Stat Scripts/characterStats.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class characterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+
+
+    public stat damage;
+    public stat armor;
+
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+
+
+
+
+    //Debug to test received damage
+    private void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+
+        }
+
+    }
+    
+    
+    public void TakeDamage (int damage)
+    {
+        //prevents some damage depending on armor stats
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage ");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    public virtual void Die ()
+    {
+        //Death happens
+        Debug.Log(transform.name + " died ");
+
+
+    }
+}
---
+++ Assets/Scripts/Stat Scripts/stat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class stat {
+
+    [SerializeField]
+    private int baseValue;
+
+    public int GetValue ()
+    {
+        return baseValue;
+    }
+
+
+
+
+
+
+}
